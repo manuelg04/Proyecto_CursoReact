947e3abbc22253cda43994bd6310349b8d6ab424
Manuel Gonzalez <manuelg9704@gmail.com>
2023-01-05 14:07:59 -0500

    Creando la pagina de registrar

--- frontend/src/paginas/Login.jsx
+++ frontend/src/paginas/Login.jsx
@@ -11,14 +11,14 @@ const Login = () => {
     <form className="my-10 bg-white shadow rounded-lg px-10 py-10">
         <div className="my-5">
             <label className="uppercase text-gray-600 block text-xl font-bold"> Email</label>
-            <input type="email" placeholder="Email de registro" className="w-full mt-3 p-3 border rounded-xl bg-gray-50">
+            <input id="email" type="email" placeholder="Email de registro" className="w-full mt-3 p-3 border rounded-xl bg-gray-50">
             
             </input>
         </div>
 
         <div className="my-5">
             <label className="uppercase text-gray-600 block text-xl font-bold"> Password</label>
-            <input id="email" type="password" placeholder="Ingresa tu password" className="w-full mt-3 p-3 border rounded-xl bg-gray-50">
+            <input id="password" type="password" placeholder="Ingresa tu password" className="w-full mt-3 p-3 border rounded-xl bg-gray-50">
             
             </input>
         </div>
--- frontend/src/paginas/Registrar.jsx
+++ frontend/src/paginas/Registrar.jsx
@@ -1,7 +1,59 @@
+import { Link } from "react-router-dom"
 
 const Registrar = () => {
   return (
-    <div>Registrar</div>
+    <>
+    
+    <h1 className="text-sky-600 font-black text-6xl capitalize">Crea tu cuenta y administra tus
+     <span className="text-slate-700"> proyectos </span>
+    </h1>
+
+    <form className="my-10 bg-white shadow rounded-lg px-10 py-10">
+
+    <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold"> Nombre </label>
+            <input type="text" placeholder="Tu nombre" className="w-full mt-3 p-3 border rounded-xl bg-gray-50">
+            
+            </input>
+        </div>
+
+        <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold"> Email</label>
+            <input id="email" type="email" placeholder="Email de registro" className="w-full mt-3 p-3 border rounded-xl bg-gray-50">
+            
+            </input>
+        </div>
+
+        <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold"> Password</label>
+            <input id="password" type="password" placeholder="Ingresa tu password" className="w-full mt-3 p-3 border rounded-xl bg-gray-50">
+            
+            </input>
+        </div>
+
+        <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold"> Repite Tu Password</label>
+            <input id="password2" type="password" placeholder="Repite tu password" className="w-full mt-3 p-3 border rounded-xl bg-gray-50">
+            
+            </input>
+        </div>
+
+        <input type="submit" value="Crear cuenta" className="bg-sky-700 w-full mb-5 py-3 text-white uppercase font-bold rounded hover:cursor-pointer">
+
+            
+        </input>
+    </form>
+
+    <nav className="lg:flex lg:justify-between">
+        <Link to="/" className="block text-center my-5 text-slate-500 uppercase">
+        Si ya tienes cuenta puedes iniciar sesion
+        </Link>
+
+        <Link to="olvide-password" className="block text-center my-5 text-slate-500 uppercase">
+        Olvide mi password
+        </Link>
+    </nav>
+    </>
   )
 }
 
